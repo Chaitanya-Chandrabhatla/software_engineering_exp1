2ded54ffded859576b87cd4ae63381ccf7baffc0
Chaitanya Chandrabhatla <chaitanyachandrabhatla@gmail.com>
2025-12-11 21:47:35 +0530

    initial commit

---
+++ sample.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+
+
+
+#class
+
+class employee:
+    name = "Chicken"
+    language = "Python"
+    salary = 100000
+
+
+Chicken = employee
+
+print(Chicken.name, Chicken.salary, Chicken.language)
